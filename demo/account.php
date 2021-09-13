722cfc238705883cbc38a2fb571fe32e3bb2eeb4
thinkbim <thinkbim@thinkbim.cn>
2021-09-13 15:02:49 +0800

    mylogger conf

--- demo/account.php
+++ demo/account.php
@@ -4,6 +4,13 @@ use ThinkBIM\UCSDK\HCClient;
 
 require "../vendor/autoload.php";
 
-$account = new HCClient('账户名称', '密码', 'token', 'target');
-$client->account->getAccount();
+$client = new HCClient([
+    'username' => '账户名称',
+    'password' => '账户密码',
+    'token' => '申请汇川API时得到的token值',
+    // 'target' => 'target字段表示被该账户管家或者代理商管辖和操作的账户名'
+]);
+
+//获取当前账户管家或者代理商下的子账户列表
+$client->account->getChildrenAccountByAccountId();
 
